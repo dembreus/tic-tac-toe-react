fe400d751c5e8d5f70b5b117bc66027bf5d45c0c
Unknown <dillon.embreus@gmail.com>
2018-08-06 17:27:03 -0600

    Started tic tac toe tutorial with react

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+registerServiceWorker();
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
+}
+
+render()
+{
+    return (
+        <button className="square"
+                onClick={() => this.setState({value: 'X'})}>
+            {this.state.value}
+        </button>
+    )
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                </div>
+            </div>
+        )
+    }
+
+    renderSquare(i) {
+        return <Square value={i}>
+            }
+            }
